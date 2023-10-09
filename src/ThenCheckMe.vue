9768240923c7a029c8ddbe805be4e0ce646727f7
András Parditka <aparditka@ovitas.hu>
2023-10-09 17:00:36 +0200

    Initial version.

---
+++ src/ThenCheckMe.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { renameMe } from './renameMe';
+
+</script>
+
+<template>
+  {{renameMe()}}
+</template>
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import ThenCheckMe from './ThenCheckMe.vue'
+
+createApp(ThenCheckMe).mount('#app')
